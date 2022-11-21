3b40aaa274df622e6a64bc2f2be6517a9bfcd0fd
wdev11 <wdev1@protonmail.com>
2022-11-21 14:27:04 +0100

    Added google tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,20 +9,32 @@ import '../styles/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import Layout from 'components/Layout'
+import Script from 'next/script'
 config.autoAddCss = false
 
 const Starkboard = ({ Component, pageProps }: AppProps) => {
   return (
-      <Provider store={wrapper}>
-        <NextHead>
-          <title>Starkboard | Find all the latest Starknet data that you are looking for</title>
-          <link rel="icon" href="favicon.ico" />
-          <link href="https://api.fontshare.com/v2/css?f[]=clash-display@700,600,300&display=swap" rel="stylesheet" />
-        </NextHead>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Provider>
+    <Provider store={wrapper}>
+      <NextHead>
+        <title>Starkboard | Find all the latest Starknet data that you are looking for</title>
+        <link rel="icon" href="favicon.ico" />
+        <link href="https://api.fontshare.com/v2/css?f[]=clash-display@700,600,300&display=swap" rel="stylesheet" />
+
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-68DNMJFBK4"></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-68DNMJFBK4');
+        `}
+        </Script>
+      </NextHead>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   )
 }
 
