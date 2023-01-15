02b109470a80b773323dee9f7a288f4c14b1bcea
Jacky LIM <j4cky.lm@gmail.com>
2023-01-15 23:33:53 +0100

    feat: V2 tvl

--- src/components/Tables/TVL.tsx
+++ src/components/Tables/TVL.tsx
@@ -1,3 +1,4 @@
+import { Selector } from 'components/Selector/Selector'
 import { sum } from 'lodash'
 import Image from 'next/image'
 import React, { useEffect, useMemo, useState } from 'react'
@@ -25,6 +26,44 @@ tokensTypes.set('usdc', 'Stablecoin')
 tokensTypes.set('usdt', 'Stablecoin')
 tokensTypes.set('strk', 'Layer 2')
 
+enum AppType {
+  ALL = 'ALL',
+  DAPP = 'DAPP',
+  LAYER2 = 'LAYER2'
+}
+
+enum Period {
+  WEEKLY = 'WEEKLY',
+  MONTHLY = 'MONTHLY',
+  TRIMESTER = 'TRIMESTER',
+  SEMESTER = 'SEMESTER',
+  YEARLY = 'YEARLY'
+}
+
+const appTypeMappingToName: Record<AppType, string> = {
+  [AppType.ALL]: 'All',
+  [AppType.DAPP]: 'Dapps',
+  [AppType.LAYER2]: 'Layer 2'
+}
+
+const periodMappingToDay: Record<Period, string> = {
+  [Period.WEEKLY]: '7D',
+  [Period.MONTHLY]: '30D',
+  [Period.TRIMESTER]: '90D',
+  [Period.SEMESTER]: '180D',
+  [Period.YEARLY]: '365D'
+}
+
+const appTypeOptions = Object.keys(AppType).map(key => ({
+  value: appTypeMappingToName[key as AppType],
+  originalValue: key as AppType
+}))
+
+const periodOptions = Object.keys(Period).map(key => ({
+  value: periodMappingToDay[key as Period],
+  originalValue: key as Period
+}))
+
 const TvlTable = () => {
   const tvlUnits = useSelector<RootState, TvlUnit[]>(state => state.tvlEvolution.data)
   const prices = useSelector<RootState, TokenPricesState>(state => state.tokensPrices)
@@ -32,6 +71,8 @@ const TvlTable = () => {
   const totalTVL = useMemo(() => {
     return sum(tvlUnits.map(tvlUnit => tvlUnit.total))
   }, [tvlUnits])
+  const [appType, setAppType] = useState<string>(AppType.ALL)
+  const [period, setPeriod] = useState<string>(Period.SEMESTER)
 
   useEffect(() => {
     if (tvlUnits.length > 2) {
@@ -52,29 +93,42 @@ const TvlTable = () => {
       setOrderedTokens(orderedTokens.sort((a, b) => (b.currentTvl - a.currentTvl)))
     }
   }, [tvlUnits, prices])
+
+  const handleAppTypeChange = (option: string) => {
+    setAppType(option)
+  }
+
+  const handlePeriodChange = (option: string) => {
+    setPeriod(option)
+  }
+
   return (
-    <div className="card table-container">
-      <div className="table-responsive">
-        <table className="table table-bordered data-table text-center tvl-table table-striped text-white">
+    <div className="card table-container data-table py-4">
+      <div className="flex flex-row justify-between px-4">
+        <Selector currentOption={appType} options={appTypeOptions} onChange={handleAppTypeChange} />
+        <Selector currentOption={period} options={periodOptions} onChange={handlePeriodChange} />
+      </div>
+      <div className="table-responsive mt-2">
+        <table className="table  text-center tvl-table text-white">
           <thead>
             <tr className="text-uppercase">
-              <th className="d-flex justify-start">Assets</th>
+              <th className="d-flex justify-start ml-5">Assets</th>
               <th className="d-none d-lg-table-cell">Percentage</th>
               <th className="d-none d-lg-table-cell">24h Change</th>
-              <th>TVL</th>
+              <th className="d-flex">TVL</th>
             </tr>
           </thead>
           <tbody>
             {
               orderedTokens.map((token, index) => (
                 <tr className="text-white" key={token.name}>
-                  <td className="d-flex flex-row align-items-center justify-content-between">
+                  <td className="d-flex flex-row align-items-center justify-content-between ml-5">
                     <div className="d-flex flex-row">
                       <div className="mx-3">{index + 1}</div>
                       <div className="d-flex flex-row">
                         <Image src={`/images/tokens/${token.name}.png`} height={20} width={20} alt={`${token.name} Logo`} className="d-flex " />
                         <span className="ml-4">
-                         {token.name.toUpperCase()}
+                        {token.name.toUpperCase()}
                         </span>
                       </div>
                     </div>
@@ -86,7 +140,7 @@ const TvlTable = () => {
                       {isNaN(token.change) ? '0' : token.change.toFixed(1)}%
                     </span>
                   </td>
-                  <td>{formatCurrency(token.currentTvl, 0)}</td>
+                  <td className="d-flex">{formatCurrency(token.currentTvl, 0)}</td>
                 </tr>
               ))
             }
